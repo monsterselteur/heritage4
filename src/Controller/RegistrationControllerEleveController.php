e0d6ea8cb14fd687b5a48eeb2a5a6a36724bc85d
Theop <68582445+amounille@users.noreply.github.com>
2023-04-20 17:15:39 +0200

    ajout bootstrapp + authentification eleve

---
+++ src/Controller/RegistrationControllerEleveController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User1;
+use App\Form\User1Type;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationControllerEleveController extends AbstractController
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/registration/eleve", name="registration")
+     */
+    public function index(Request $request)
+    {
+        $user = new User1();
+
+        $form = $this->createForm(User1Type::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encode the new users password
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()));
+
+            // Set their role
+            $user->setRoles(['ROLE_USER']);
+
+            // Save
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration_controller_eleve/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}

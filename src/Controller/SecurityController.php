366bc7dd8d9fb7b052e6f64f5154a582075fde86
Abdelsadok Ilies <ilies.abdelsadok@gmail.com>
2023-04-27 09:22:09 +0200

    security controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,7 +18,7 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('accueil/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
 
 
     }

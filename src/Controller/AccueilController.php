778b3739e8d9fee7d4c253e6f1c67dfcaba81820
Abdelsadok Ilies <ilies.abdelsadok@gmail.com>
2023-04-27 20:41:17 +0200

    accueil eleve

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\CompetenceRepository;
 use App\Repository\EleveRepository;
+use App\Repository\SituationRepository;
+use App\Repository\User1Repository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,20 +15,34 @@ class AccueilController extends AbstractController
     /**
      * @Route("/accueil", name="app_accueil")
      */
-    public function index(EleveRepository $eleveRepository): Response
+    public function index(EleveRepository $eleveRepository, SituationRepository $situationRepository): Response
     {
-        $eleves = $eleveRepository->findAll();
+        $situations = $situationRepository->findAll();
+        $users = $eleveRepository->findAll();
         $eleve = array();
-        foreach ($eleves as $eleveItem) {
+        foreach ($users as $eleveItem) {
             if (in_array('ROLE_USER', $eleveItem->getRoles())) {
                 $eleve[] = $eleveItem;
             }
         }
 
+        $pasadmin = array();
+        foreach ($users as $eleveItem) {
+            if (in_array('ROLE_USER', $eleveItem->getRoles())) {
+                $pasadmin[] = $eleveItem;
+            }
+            if (in_array('ROLE_PROF', $eleveItem->getRoles())) {
+                $pasadmin[] = $eleveItem;
+            }
+
+        }
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
-            'eleves' => $eleve
-
+            'eleves' => $eleve,
+            'users' => $pasadmin,
+            'situation' => $situations
         ]);
     }
+
 }
